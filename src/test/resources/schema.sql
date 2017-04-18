ea4ab111bf730aa9bd0cd50c70b2c2522b5a4a64
Manuel de Dios Tuero <manutuero@gmail.com>
2017-04-17 21:17:18 -0300

    Update schema.sql

--- src/test/resources/schema.sql
+++ src/test/resources/schema.sql
@@ -1,9 +1,10 @@
-DROP TABLE usuario IF EXISTS;
+DROP TABLE IF EXISTS usuario;
 
 CREATE TABLE usuario
 ( username VARCHAR(25),
   password VARCHAR(8),
-  activo BOOLEAN DEFAULT True
+  activo BOOLEAN DEFAULT true
 );
 
 INSERT INTO usuario (username, password, activo) VALUES ('manu','1234',true);
+INSERT INTO usuario (username, password, activo) VALUES ('gabi','5678',true);
