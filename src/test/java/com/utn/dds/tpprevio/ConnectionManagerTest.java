76ca01ccd7532ddcda07980d7328d0d64b30dd0b
Manuel de Dios Tuero <manutuero@gmail.com>
2017-04-17 21:27:27 -0300

    Update ConnectionManagerTest.java

--- src/test/java/com/utn/dds/tpprevio/ConnectionManagerTest.java
+++ src/test/java/com/utn/dds/tpprevio/ConnectionManagerTest.java
@@ -7,13 +7,13 @@ import com.utn.dds.tpprevio.repository.impl.ConnectionManager;
 public class ConnectionManagerTest {	
 
 	@Test
-	public void getMYSQLConnection_conParametrosValidos_devuelveConexion() throws ClassNotFoundException {
+	public void getConnection_conParametrosValidos_devuelveConexion() throws ClassNotFoundException {
 		assertEquals("com.mysql.jdbc.JDBC4Connection",
 				ConnectionManager.getConnection().getClass().getName());
 	}
 	
 	@Test(expected = ClassNotFoundException.class)
-	public void getMYSQLConnection_conParametrosNoValidos_devuelveExcepcion() throws ClassNotFoundException {
+	public void getConnection_conParametrosNoValidos_devuelveExcepcion() throws ClassNotFoundException {
 		assertNotNull(ConnectionManager.getConnection("url no valida"," driver no valido"));
 	}
 }
