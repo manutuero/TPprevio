bcb1fab942626499ba3c582783f5c74b61398761
Manuel Tuero <manutuero@gmail.com>
2017-04-25 20:03:20 -0300

    se agrega CountryRepositoryImpl

--- src/main/java/com/utn/dds/tpprevio/repository/CountryRepository.java
+++ src/main/java/com/utn/dds/tpprevio/repository/CountryRepository.java
@@ -1,6 +1,8 @@
 package com.utn.dds.tpprevio.repository;
 
+import com.utn.dds.tpprevio.domain.Country;
+
 public interface CountryRepository {
-	public void agregarPais(String codigo_pais,String nombre_pais);
-	public String buscarPaisPorId(String idPais);
+	public void agregar(String codigo_pais, String nombre_pais);
+	public Country buscarPorId(String idPais);
 }
--- src/test/java/com/utn/dds/tpprevio/CountryRepositoryImplTest.java
+++ src/test/java/com/utn/dds/tpprevio/CountryRepositoryImplTest.java
@@ -9,10 +9,10 @@ import com.utn.dds.tpprevio.repository.impl.CountryRespositoryImpl;;
 public class CountryRepositoryImplTest {
 	
 	private CountryRepository countryRepository = new CountryRespositoryImpl();
+	
 	@Test
-	public void agregarPais() {
-	  countryRepository.agregarPais("AR", "Argentina");
-	  assertEquals("Argentina",countryRepository.buscarPaisPorId("AR"));
+	public void agregar_conPaisValido_agregaCorrectamente() {
+	  countryRepository.agregar("AR", "Argentina");
+	  assertEquals("Argentina",countryRepository.buscarPorId("AR").getName());
 	}
-
 }
