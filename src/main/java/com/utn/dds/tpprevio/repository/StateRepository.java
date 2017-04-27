c892c76bb9596ac7ba723d1ac8c688fa65341d89
Manuel Tuero <manutuero@gmail.com>
2017-04-27 01:07:56 -0300

    se agrega boton actualizarBase

--- src/main/java/com/utn/dds/tpprevio/repository/StateRepository.java
+++ src/main/java/com/utn/dds/tpprevio/repository/StateRepository.java
@@ -1,7 +1,11 @@
 package com.utn.dds.tpprevio.repository;
+import java.util.List;
+
+import com.utn.dds.tpprevio.domain.Country;
 import com.utn.dds.tpprevio.domain.State;
 
 public interface StateRepository {
 	public void agregar(String codigo_state, String nombre_state, String fk_country);
 	public State buscarPorId(String idState);
+	public List<State> buscarStates(Country country);
 }
--- src/main/java/com/utn/dds/tpprevio/service/StateService.java
+++ src/main/java/com/utn/dds/tpprevio/service/StateService.java
@@ -1,6 +1,12 @@
 package com.utn.dds.tpprevio.service;
+
+import java.util.List;
+
+import com.utn.dds.tpprevio.domain.Country;
 import com.utn.dds.tpprevio.domain.State;
-public interface  StateService {
-		public void agregarPais(String codigo_state, String nombre_state,String fk_state);
-		public State buscarPorId(String idState);
-	}
+
+public interface StateService {
+	public void agregarPais(String codigo_state, String nombre_state, String fk_state);
+	public State buscarPorId(String idState);
+	public List<State> buscarStates(Country country);
+}
